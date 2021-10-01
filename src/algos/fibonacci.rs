97fff6b19288ce9d49fb6d783ce2a3f514b6867a
Stefano Torresi <stefano@torresi.io>
2021-10-01 16:28:45 +0200

    more correct fibonacci

--- src/algos/fibonacci.rs
+++ src/algos/fibonacci.rs
@@ -1,8 +1,37 @@
 pub fn fibonacci(n: u128) -> u128 {
-    if n < 2 {
+    if n <= 1 {
+        return 0
+    }
+    if n == 2 {
         return 1
     }
     return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+static mut FIBONACCI_NUMBERS: Vec<u128> = Vec::new();
+
+pub unsafe fn fibonacci_iterative(n: usize) -> u128 {
+    let result = FIBONACCI_NUMBERS.get(n);
+    if result.is_some() {
+        return *result.unwrap()
+    }
+
+    let last = FIBONACCI_NUMBERS.len();
+    for i in last..=n {
+        if i <= 1 {
+            FIBONACCI_NUMBERS.push(0);
+            continue
+        }
+
+        if i == 2 {
+            FIBONACCI_NUMBERS.push(1);
+            continue
+        }
+
+        FIBONACCI_NUMBERS.push(FIBONACCI_NUMBERS[i - 1] + FIBONACCI_NUMBERS[i - 2]);
+    }
+
+    return FIBONACCI_NUMBERS[n];
 }
 
 #[cfg(test)]
@@ -11,12 +40,30 @@ mod tests {
 
     #[test]
     fn test_fibonacci() {
-        assert_eq!(1, fibonacci(0));
-        assert_eq!(1, fibonacci(1));
-        assert_eq!(2, fibonacci(2));
-        assert_eq!(3, fibonacci(3));
-        assert_eq!(5, fibonacci(4));
-        assert_eq!(8, fibonacci(5));
-        assert_eq!(13, fibonacci(6));
+        assert_eq!(0, fibonacci(0));
+        assert_eq!(0, fibonacci(1));
+        assert_eq!(1, fibonacci(2));
+        assert_eq!(1, fibonacci(3));
+        assert_eq!(2, fibonacci(4));
+        assert_eq!(3, fibonacci(5));
+        assert_eq!(5, fibonacci(6));
+        assert_eq!(8, fibonacci(7));
+        assert_eq!(13, fibonacci(8));
+    }
+
+    #[test]
+    fn test_fibonacci_memoized() {
+        unsafe {
+            assert_eq!(63245986, fibonacci_memoized(40));
+            assert_eq!(0, fibonacci_memoized(0));
+            assert_eq!(0, fibonacci_memoized(1));
+            assert_eq!(1, fibonacci_memoized(2));
+            assert_eq!(1, fibonacci_memoized(3));
+            assert_eq!(2, fibonacci_memoized(4));
+            assert_eq!(3, fibonacci_memoized(5));
+            assert_eq!(5, fibonacci_memoized(6));
+            assert_eq!(8, fibonacci_memoized(7));
+            assert_eq!(13, fibonacci_memoized(8));
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::time::Instant;
+use hello_rust::algos::fibonacci::*;
+
+fn benchmark<F: Fn()>(f: F) {
+    let now = Instant::now();
+    f();
+    println!("{:?}", now.elapsed())
+}
+
 fn main() {
-    println!("hello world!")
+    benchmark(|| {
+        println!("{}", fibonacci(50));
+    });
+    benchmark(|| {
+        unsafe {
+            println!("{}", fibonacci_iterative(150));
+        }
+    });
+    benchmark(|| {
+        unsafe {
+            println!("{}", fibonacci_iterative(150));
+        }
+    });
 }
